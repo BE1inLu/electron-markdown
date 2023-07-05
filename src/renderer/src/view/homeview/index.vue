6e46066ae94afeca606e0710c6ea9af110a4d02a
TeufulNeko <enluba2333@gmail.com>
2023-07-05 20:30:55 +0800

    update

--- src/renderer/src/view/homeview/index.vue
+++ src/renderer/src/view/homeview/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>homeview</div>
+  <div>
+    <!-- homeview -->
+    <h2>version</h2>
+    <versions></versions>
+  </div>
 </template>
 
 <script>
+import versions from '../../components/Versions.vue'
 export default {
-
+  components:{versions}
 }
 </script>
 
